c00e962b37003578c191a52289516430318a6e5d
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-16 13:29:35 -0600

    chore: :lipstick: reset style add variables and menu

---
+++ ESFE.FabLab.DAL/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.FabLab.DAL.Repositories
+{
+    public interface IGenericRepository<T> where T:class
+    {
+        Task<bool> Create(T model);
+        Task<bool> Update(T model);
+        Task<bool> Delte(int id);
+        Task<IQueryable<T>> GetAll();
+    }
+}
--- ESFE.FabLab.UI/wwwroot/js/site.js
+++ ESFE.FabLab.UI/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const hamburger = document.querySelector(".hamburger");
+const modal = document.querySelector(".bg-modal");
+const navbar = document.querySelector(".navbar");
 
-// Write your JavaScript code.
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger-active")
+    modal.classList.toggle("bg-modal-active")
+    navbar.classList.toggle("navbar-active")
+})
