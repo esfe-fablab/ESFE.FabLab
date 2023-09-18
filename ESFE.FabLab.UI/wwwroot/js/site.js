6ee0a93c236d33ec0f1ea52c2ce1edb4dc83c407
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-17 18:12:40 -0600

    chore: :sparkles: change menu style when scroll

--- ESFE.FabLab.UI/wwwroot/js/site.js
+++ ESFE.FabLab.UI/wwwroot/js/site.js
@@ -7,3 +7,25 @@ hamburger.addEventListener("click", () => {
     modal.classList.toggle("bg-modal-active")
     navbar.classList.toggle("navbar-active")
 })
+
+
+// Obtener una referencia al elemento
+const miElemento = document.querySelector(".navbar")
+
+// Agregar un evento de desplazamiento (scroll) al objeto window
+window.addEventListener('scroll', () => {
+    // Obtener la posición actual del scroll
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition)
+    // Define la posición de scroll en la que deseas agregar o quitar la clase
+    const posicionDeseada = 300; // Cambia esto según tu necesidad
+
+    // Comprobar si la posición de scroll es mayor o igual a la posición deseada
+    if (scrollPosition >= posicionDeseada) {
+        // Si es así, agrega la clase
+        miElemento.classList.add('navbar-active');
+    } else {
+        // Si no, quita la clase
+        miElemento.classList.remove('navbar-active');
+    }
+});
